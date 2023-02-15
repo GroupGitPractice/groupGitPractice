70ab3d88390aec432426638f65994797d631c620
Ubin Jung <binimutter@gmail.com>
2023-02-15 16:36:48 -0600

    final edits

--- Program.cs
+++ Program.cs
@@ -1,62 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Create an list and loop through
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Dictionary<type, type> name = new Dictionary<type, type>();
-Dictionary<string, string> favFoods = new Dictionary<string, string>();
-favFoods.Add("rice", "Nigerian Jollof Rice");
-favFoods.Add("soup", "Goat Meat Pepper Soup");
-favFoods.Add("appetizer", "Spicy Tuna Sushi Roll");
-
-foreach(var entry in favFoods) {
-    Console.WriteLine($"My favorite {entry.Key} is {entry.Value}");
-}
 List<string> favoriteFoodList = new List<string>();
 favoriteFoodList.Add("Pasta");
 favoriteFoodList.Add("Sushi");
@@ -69,12 +13,21 @@ foreach (var food in favoriteFoodList)
     Console.WriteLine("I LOVE " + food);
 }
 
-
 // Create an array and loop through
 
-string[] favFoods = { "Pizza rolls", "Ramen", "Wings", "Sushi", "Fruit" };
+string[] favFoodsArray = { "Pizza rolls", "Ramen", "Wings", "Sushi", "Fruit" };
 
-for (int i = 0; i < favFoods.Length; i++)
+for (int i = 0; i < favFoodsArray.Length; i++)
 {
-    Console.WriteLine(favFoods[i]);
+    Console.WriteLine(favFoodsArray[i]);
+}
+
+// Dictionary<type, type> name = new Dictionary<type, type>();
+Dictionary<string, string> favFoodsDictionary = new Dictionary<string, string>();
+favFoodsDictionary.Add("rice", "Nigerian Jollof Rice");
+favFoodsDictionary.Add("soup", "Goat Meat Pepper Soup");
+favFoodsDictionary.Add("appetizer", "Spicy Tuna Sushi Roll");
+
+foreach(var entry in favFoods) {
+    Console.WriteLine($"My favorite {entry.Key} is {entry.Value}");
 }
