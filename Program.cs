97a174493f245819119db9578593a16a7c2ab94b
Aquila Olose <aquilaolose@Aquilas-Air.home>
2023-02-15 16:08:23 -0600

    created dictionary

--- Program.cs
+++ Program.cs
@@ -1,2 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Dictionary<type, type> name = new Dictionary<type, type>();
+Dictionary<string, string> favFoods = new Dictionary<string, string>();
+favFoods.Add("rice", "Nigerian Jollof Rice");
+favFoods.Add("soup", "Goat Meat Pepper Soup");
+favFoods.Add("appetizer", "Spicy Tuna Sushi Roll");
+
+foreach(var entry in favFoods) {
+    Console.WriteLine($"My favorite {entry.Key} is {entry.Value}");
+}
