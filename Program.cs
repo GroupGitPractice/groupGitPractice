8a93ce13eebe24ea68e58905064ed9886325a5ce
Ubin Jung <binimutter@gmail.com>
2023-02-15 15:44:53 -0600

    list and loop through

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+List<string> favoriteFoodList = new List<string>();
+favoriteFoodList.Add("Pasta");
+favoriteFoodList.Add("Sushi");
+favoriteFoodList.Add("Fries");
+favoriteFoodList.Add("Fajitas");
+favoriteFoodList.Add("Poke Bowls");
+
+foreach (var food in favoriteFoodList)
+{
+    Console.WriteLine("I LOVE " + food);
+}
